875e85d01fdd4fda1fff2b259888e9f39fe98ae8
Yogesh Shete <shete.yogesh101@gmail.com>
2024-09-17 11:09:15 +0530

    Added initial files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'about',
+    component:AboutMeComponent
+  }
+];
 
 @NgModule({
+  // declarations:[AboutMeComponent],
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
