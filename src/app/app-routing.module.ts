e86aa1b791481f047407e34733a75d9e96ca03ae
Yogesh Shete <shete.yogesh101@gmail.com>
2024-09-23 00:26:57 +0530

    added scrolling web effect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
 
 const routes: Routes = [
   { path: 'about', component: AboutMeComponent },
+  { path: 'contactme', component: ContactMeComponent }
   // { path: '**', redirectTo: 'about' } // Redirect to about page for unknown routes
 ];
 
