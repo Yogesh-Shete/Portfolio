930fe2355ee51a959c18ac2530ae4e548eb67d55
Yogesh Shete <shete.yogesh101@gmail.com>
2025-03-12 23:12:15 +0530

    Added A19 changes

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-contact-me',
-  templateUrl: './contact-me.component.html',
-  styleUrls: ['./contact-me.component.css']
+    selector: 'app-contact-me',
+    templateUrl: './contact-me.component.html',
+    styleUrls: ['./contact-me.component.css'],
+    standalone: false
 })
 export class ContactMeComponent {
 
